d692ef9fa29f9003b215ce486a567be538eebc9f
Agung Cahya <agungcahya122@gmail.com>
2023-03-05 20:44:19 +0700

    feat: finish responsive login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -85,30 +85,30 @@ const Navbar = () => {
 
         <input type="checkbox" id="my-modal-3" className="modal-toggle" />
         <div className="modal">
-          <div className="modal-box relative w-8/12 max-w-full bg-color4">
+          <div className="modal-box relative w-10/12 max-w-full bg-color4 md:w-10/12 lg:w-8/12">
             <label
               htmlFor="my-modal-3"
               className="TitleShadow rounded-ful btn-ghost btn absolute right-2 top-2 rounded-3xl text-[18px] font-bold text-color1"
             >
               ✕
             </label>
-            <img src={Login} alt="login.svg" className="w-16" />
+            <img src={Login} alt="login.svg" className="w-10 md:w-14 lg:w-16" />
 
-            <p className="TitleShadow -mt-5 text-center text-[32px] font-bold text-color1 ">
+            <p className="TitleShadow -mt-5 text-center text-[26px] font-bold text-color1 md:text-[32] lg:text-[32px] ">
               Login
             </p>
-            <p className="TitleShadow md:mx- mt-4 border-b-2 border-dashed pb-4 text-center text-[18px] text-color1 lg:mx-44">
+            <p className="TitleShadow mx-5 mt-4 border-b-2 border-dashed pb-4 text-center text-[15px] text-color1 md:mx-20 md:text-[18px] lg:mx-44 lg:text-[18px]">
               Login for your account and lets enjoy your website
             </p>
 
-            <div className="mt-16 flex pl-20">
+            <div className="mt-16 flex flex-col gap-5 pl-2 md:flex-row md:gap-0 md:pl-10 lg:flex-row lg:gap-0 lg:pl-20">
               <div className="flex items-center gap-4">
                 <p className="text-[16px] font-semibold">E-mail :</p>
                 <InputCustom
                   id="input-nama"
                   type="text"
                   placeholder="admin@gmail.com"
-                  className="input-border input h-8 w-8/12 max-w-full rounded-2xl border-2 border-zinc-500 bg-color1 px-4 py-4 text-[16px] text-color5 placeholder-slate-400"
+                  className="input-border input h-8 w-8/12 max-w-full rounded-2xl border-2 border-zinc-500 bg-color1 px-4 py-4 text-[14px] text-color5 placeholder-slate-400 md:text-[15px] lg:text-[16px]"
                 />
               </div>
 
@@ -118,17 +118,17 @@ const Navbar = () => {
                   id="input-nama"
                   type="text"
                   placeholder="*********"
-                  className="input-border input h-8 w-6/12 max-w-full rounded-2xl border-2 border-zinc-500 bg-color1 px-4 py-4 text-[16px] text-color5 placeholder-slate-400"
+                  className="input-border input h-8 w-7/12 max-w-full rounded-2xl border-2 border-zinc-500 bg-color1 px-4 py-4 text-[14px] text-color5 placeholder-slate-400 md:w-6/12 md:text-[15px] lg:w-6/12 lg:text-[16px]"
                 />
               </div>
             </div>
             <CustomButton
               id="btn-login"
               label="Login"
-              className="mt-10 ml-[27vw] rounded-2xl bg-color6 px-10 py-1 text-[16px] font-semibold text-white shadow-[1px_2px_4px_0px_rgba(0,0,0,0.5)] hover:bg-[rgb(0,140,255)] disabled:cursor-not-allowed disabled:bg-color2"
+              className="mt-10 ml-[20vw] rounded-2xl bg-color6 px-10 py-1 text-[16px] font-semibold text-white shadow-[1px_2px_4px_0px_rgba(0,0,0,0.5)] hover:bg-[rgb(0,140,255)] disabled:cursor-not-allowed disabled:bg-color2 md:ml-[30vw] lg:ml-[27vw]"
             />
 
-            <p className="mt-5 text-end text-[16px] text-color1">
+            <p className="mt-5 text-end text-[14px] text-color1 md:text-[15px] lg:text-[16px]">
               (just admin)
             </p>
           </div>
