e9840cc492fb1a307ee89fa1b772a593640fbad9
pratama <alfin.pratama@ottodigital.id>
2023-03-05 18:58:00 +0700

    add fetchdata memberlist

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -5,7 +5,7 @@ import Logo from "../../assets/logo.svg";
 const Login = () => {
   return (
     <Layout>
-      {/* <div className="">
+      <div className="">
         <img src={Logo} alt="login.svg" className="w-16" />
 
         <p className="-mt-5 text-center text-[32px] font-bold text-color1">
@@ -14,7 +14,7 @@ const Login = () => {
         <p className="ml-44 mr-44 mt-4 border-b-2 border-dashed pb-4 text-center text-[18px] text-color1">
           Login for your account and lets enjoy your website
         </p>
-      </div> */}
+      </div>
     </Layout>
   );
 };
