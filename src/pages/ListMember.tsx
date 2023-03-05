3d778d65923b6c271a9e0cb4042186aa6ba8d78e
pratama <alfin.pratama@ottodigital.id>
2023-03-06 01:30:32 +0700

    conflict listmember

--- src/pages/ListMember.tsx
+++ src/pages/ListMember.tsx
@@ -72,71 +72,68 @@ const ListMember = () => {
         List Peserta JarFisya
       </p>
 
-      <div className="mt-16 flex flex-row items-center justify-center px-20">
-        <div className="flex w-[22rem] gap-2 rounded-full bg-color4 py-2 px-6">
+
+      <div className="mt-16 flex flex-col-reverse items-center justify-center px-4 md:flex-row md:px-10 lg:flex-row lg:px-20">
+        <div className="mt-8 flex w-[22rem] gap-2 rounded-full bg-color4 py-2 px-6 md:mt-0 lg:mt-0">
           <input
-            onChange={(e) => setSearch(e.target.value)} />
-          <div className="mt-16 flex flex-col-reverse items-center justify-center px-4 md:flex-row md:px-10 lg:flex-row lg:px-20">
-            <div className="mt-8 flex w-[22rem] gap-2 rounded-full bg-color4 py-2 px-6 md:mt-0 lg:mt-0">
-              <InputCustom
-                id="input-search"
-                type="search"
-                placeholder="Mencari Nama Peserta .... ?"
-                className="input-border input h-8 w-full max-w-full rounded-full bg-color4 px-3 text-[15px] font-medium tracking-wider text-color5 placeholder-color5 md:text-[15px] lg:text-[16px]"
-              />
-              <MdSearch className="h-8 w-8 text-color5" />
-            </div>
+            onChange={(e) => setSearch(e.target.value)}
+            id="input-search"
+            type="search"
+            placeholder="Mencari Nama Peserta .... ?"
+            className="input-border input h-8 w-full max-w-full rounded-full bg-color4 px-3 text-[15px] font-medium tracking-wider text-color5 placeholder-color5 md:text-[15px] lg:text-[16px]"
+          />
+          <MdSearch className="h-8 w-8 text-color5" />
+        </div>
 
-            <div className="mt-0 ml-auto flex h-10 gap-3 rounded-xl bg-color4 py-2 px-8 text-center text-[16px] font-medium text-color5 hover:cursor-pointer hover:bg-[rgba(13,206,218,0.7)]">
-              <TfiPrinter className="h-5 w-5 text-color5" />
-              Print to Excel
-            </div>
-          </div>
-
-          <div className="mt-8 w-full px-20">
-            <table className="table w-full ">
-              <thead className="">
-                <tr>
-                  <th className="w-1/12 bg-color4 text-center text-[14px] text-color1">
-                    No
-                  </th>
-                  <th className="w-2/12 bg-color4 text-[14px] text-color1">
-                    Nama Peserta
-                  </th>
-                  <th className="w-3/12 bg-color4 text-center text-[14px] text-color1">
-                    Telepon
-                  </th>
-                  <th className="w-2/12 bg-color4 text-[14px] text-color1">
-                    E - mail
-                  </th>
-                  <th className="w-2/12 bg-color4 text-center text-[14px] text-color1">
-                    Domisili
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="border-x-2 border-[rgba(159,159,159,0.2)]">
-                {memberList.map((listValue, index) => {
-                  return (
-                    <tr key={index}>
-                      <td className="text-center">{index + 1}</td>
-                      <td className="text-center">
-                        <a onClick={() => toDetail(listValue)} >{listValue.name}</a>
-                      </td>
-                      <td className="text-center">{listValue.wa}</td>
-                      <td >{listValue.email}</td>
-                      <td className="text-center">{listValue.domicile}</td>
-                    </tr>
-                  );
-                })}
-
-              </tbody>
-            </table>
-          </div>
+        <div className="mt-0 ml-auto flex h-10 gap-3 rounded-xl bg-color4 py-2 px-8 text-center text-[16px] font-medium text-color5 hover:cursor-pointer hover:bg-[rgba(13,206,218,0.7)]">
+          <TfiPrinter className="h-5 w-5 text-color5" />
+          Print to Excel
         </div>
       </div>
 
+      <div className="mt-8 w-full px-20">
+        <table className="table w-full ">
+          <thead className="">
+            <tr>
+              <th className="w-1/12 bg-color4 text-center text-[14px] text-color1">
+                No
+              </th>
+              <th className="w-2/12 bg-color4 text-[14px] text-color1">
+                Nama Peserta
+              </th>
+              <th className="w-3/12 bg-color4 text-center text-[14px] text-color1">
+                Telepon
+              </th>
+              <th className="w-2/12 bg-color4 text-[14px] text-color1">
+                E - mail
+              </th>
+              <th className="w-2/12 bg-color4 text-center text-[14px] text-color1">
+                Domisili
+              </th>
+            </tr>
+          </thead>
+          <tbody className="border-x-2 border-[rgba(159,159,159,0.2)]">
+            {memberList.map((listValue, index) => {
+              return (
+                <tr key={index}>
+                  <td className="text-center">{index + 1}</td>
+                  <td className="text-center">
+                    <a onClick={() => toDetail(listValue)} >{listValue.name}</a>
+                  </td>
+                  <td className="text-center">{listValue.wa}</td>
+                  <td >{listValue.email}</td>
+                  <td className="text-center">{listValue.domicile}</td>
+                </tr>
+              );
+            })}
+
+          </tbody>
+        </table>
+      </div>
+
+
       <Footer />
-    </Layout>
+    </Layout >
   );
 };
 
